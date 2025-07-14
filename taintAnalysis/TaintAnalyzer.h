59179df4d7473385fdfcfd7baca6422c60004915
mahuaroy1 <mahuaroy1@users.noreply.github.com>
2025-07-14 16:19:47 -0400

    Update TaintAnalyzer.h

--- taintAnalysis/TaintAnalyzer.h
+++ taintAnalysis/TaintAnalyzer.h
@@ -1,1 +1,22 @@
 
+// Part of the LLVM Project, under the Apache License v2.0 with LLVM Exceptions.
+// See https://llvm.org/LICENSE.txt for license information.
+// SPDX-License-Identifier: Apache-2.0 WITH LLVM-exception
+//
+//===----------------------------------------------------------------------===//
+
+#ifndef LLVM_TRANSFORMS_UTILS_TAINTANALYZER_H
+#define LLVM_TRANSFORMS_UTILS_TAINTANALYZER_H
+
+#include "llvm/IR/PassManager.h"
+
+namespace llvm {
+
+class TaintAnalyzerPass : public PassInfoMixin<TaintAnalyzerPass> {
+public:
+  PreservedAnalyses run(Function &F, FunctionAnalysisManager &AM);
+};
+
+} // namespace llvm
+
+#endif // LLVM_TRANSFORMS_UTILS_TAINTANALYZER_H
